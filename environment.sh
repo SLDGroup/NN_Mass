76bb402a2863311d048f46910e4d16dc3919414f
guihong <guihong@ECE-A13808@lambda.ece.utexas.edu>
2021-03-31 17:41:30 -0500

    release

---
+++ environment.sh
@@ -0,0 +1,7 @@
+pip install tensorboardX==2.1
+pip install --extra-index-url https://developer.download.nvidia.com/compute/redist --upgrade nvidia-dali-cuda110
+pip install torch==1.8.1
+pip install torchvision==0.9.1
+pip install thop==0.0.31
+pip install matplotlib==3.4.1
+
